49befc06a9b74e86f17d63e0c11b1d7589c721bc
Ajeesh Rajan <arajan4473@altimetrik.com>
2018-11-27 22:29:15 +0530

    build files

---
+++ build/precache-manifest.9fb7d24cd2c355c84c566ea85b8d37f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "41ae3b483ffa5a983008",
+    "url": "/static/js/main.41ae3b48.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "41ae3b483ffa5a983008",
+    "url": "/static/css/main.5ef4e10e.chunk.css"
+  },
+  {
+    "revision": "d39ded508f995afbf547d205bd1576d0",
+    "url": "/index.html"
+  }
+];
